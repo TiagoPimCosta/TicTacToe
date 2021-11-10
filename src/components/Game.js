e64861f1dc49307a92e42d79a6ab9302f4975cd7
Tiago Pimenta <tiagopimcosta@gmail.com>
2021-11-10 15:52:51 +0000

    Game Working with history

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { unstable_concurrentAct } from 'react-dom/test-utils';
 import Board from './Board'
 
 export default class Game extends Component {
@@ -14,10 +13,21 @@ export default class Game extends Component {
     }
   }
 
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2)=== 0
+    })
+  }
+
   handleClick(i){
     const history = this.state.history.slice(0, this.state.stepNumber+1);
     const current = history[history.length-1];
     const squares = current.squares.slice();
+    const winner = calculateWinner(squares);
+    if(winner || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext?'X':'O';
     this.setState({
       history: history.concat({
@@ -31,13 +41,50 @@ export default class Game extends Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move?'Move to ' + move + ' turn':'Start the Game';
+      return(
+        <li key={move}>
+          <button onClick={()=>{this.jumpTo(move)}}>
+            {desc}
+          </button>
+        </li>
+      )
+    });
+    let status;
+    if(winner){
+      status = 'Winner is ' + winner;
+    }else{
+      status = 'Next Player is ' + (this.state.xIsNext?'X':'O');
+    }
 
     return (
       <div className="game">
         <div className="game-board">
           <Board onClick={(i) => this.handleClick(i)} squares={current.squares} />
         </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ul>{moves}</ul>
+        </div>
       </div>
     )
   }
+}
+
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontals
+    [0, 4, 8], [2, 4, 6], // Diagonals
+    [0, 3, 6], [1, 4, 7], [2, 5, 8] // Verticals
+  ]
+  for( let i = 0; i < lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
